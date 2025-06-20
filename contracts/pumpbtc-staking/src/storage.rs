4ef3b6a12d0189117e2870a65a053c3dc317417d
DAVID ZHANG <d12stan@hotmail.com>
2025-06-20 20:24:32 +0700

    feat: rename storage data struct

--- contracts/pumpbtc-staking/src/storage.rs
+++ contracts/pumpbtc-staking/src/storage.rs
@@ -12,7 +12,7 @@ pub(crate) const MAX_DATE_SLOT: u32 = 10;
 // Instance storage keys - for contract configuration and management data
 #[derive(Clone)]
 #[contracttype]
-pub enum InstanceDataKey {
+pub enum DataKey {
     Admin,
     PendingAdmin,
     Paused,
@@ -28,7 +28,7 @@ pub enum InstanceDataKey {
 // Persistent storage keys - for long-term global state
 #[derive(Clone)]
 #[contracttype]
-pub enum PersistentDataKey {
+pub enum StakingDataKey {
     TotalStakingAmount,
     TotalStakingCap,
     TotalRequestedAmount,
@@ -40,7 +40,7 @@ pub enum PersistentDataKey {
 // Temporary storage keys - for time-limited user data
 #[derive(Clone)]
 #[contracttype]
-pub enum TemporaryDataKey {
+pub enum PendingDataKey {
     PendingUnstakeTime(Address, u32),
     PendingUnstakeAmount(Address, u32),
 }
@@ -48,198 +48,198 @@ pub enum TemporaryDataKey {
 pub fn read_pump_token_address(e: &Env) -> Address {
     e.storage()
         .instance()
-        .get(&InstanceDataKey::PumpTokenAddress)
+        .get(&DataKey::PumpTokenAddress)
         .unwrap()
 }
 
 pub fn write_pump_token_address(e: &Env, address: &Address) {
     e.storage()
         .instance()
-        .set(&InstanceDataKey::PumpTokenAddress, address);
+        .set(&DataKey::PumpTokenAddress, address);
 }
 
 pub fn read_asset_token_address(e: &Env) -> Address {
     e.storage()
         .instance()
-        .get(&InstanceDataKey::AssetTokenAddress)
+        .get(&DataKey::AssetTokenAddress)
         .unwrap()
 }
 
 pub fn write_asset_token_address(e: &Env, address: &Address) {
     e.storage()
         .instance()
-        .set(&InstanceDataKey::AssetTokenAddress, address);
+        .set(&DataKey::AssetTokenAddress, address);
 }
 
 pub fn read_asset_decimal(e: &Env) -> u32 {
     e.storage()
         .instance()
-        .get(&InstanceDataKey::AssetDecimal)
+        .get(&DataKey::AssetDecimal)
         .unwrap_or(8)
 }
 
 pub fn write_asset_decimal(e: &Env, decimal: u32) {
     e.storage()
         .instance()
-        .set(&InstanceDataKey::AssetDecimal, &decimal);
+        .set(&DataKey::AssetDecimal, &decimal);
 }
 
 pub fn read_operator(e: &Env) -> Option<Address> {
-    e.storage().instance().get(&InstanceDataKey::Operator)
+    e.storage().instance().get(&DataKey::Operator)
 }
 
 pub fn write_operator(e: &Env, operator: &Address) {
     e.storage()
         .instance()
-        .set(&InstanceDataKey::Operator, operator);
+        .set(&DataKey::Operator, operator);
 }
 
 pub fn read_normal_unstake_fee(e: &Env) -> i128 {
     e.storage()
         .instance()
-        .get(&InstanceDataKey::NormalUnstakeFee)
+        .get(&DataKey::NormalUnstakeFee)
         .unwrap_or(0)
 }
 
 pub fn write_normal_unstake_fee(e: &Env, fee: i128) {
     e.storage()
         .instance()
-        .set(&InstanceDataKey::NormalUnstakeFee, &fee);
+        .set(&DataKey::NormalUnstakeFee, &fee);
 }
 
 pub fn read_instant_unstake_fee(e: &Env) -> i128 {
     e.storage()
         .instance()
-        .get(&InstanceDataKey::InstantUnstakeFee)
+        .get(&DataKey::InstantUnstakeFee)
         .unwrap_or(300) // default 3%
 }
 
 pub fn write_instant_unstake_fee(e: &Env, fee: i128) {
     e.storage()
         .instance()
-        .set(&InstanceDataKey::InstantUnstakeFee, &fee);
+        .set(&DataKey::InstantUnstakeFee, &fee);
 }
 
 pub fn read_only_allow_stake(e: &Env) -> bool {
     e.storage()
         .instance()
-        .get(&InstanceDataKey::OnlyAllowStake)
+        .get(&DataKey::OnlyAllowStake)
         .unwrap_or(true)
 }
 
 pub fn write_only_allow_stake(e: &Env, only_allow_stake: bool) {
     e.storage()
         .instance()
-        .set(&InstanceDataKey::OnlyAllowStake, &only_allow_stake);
+        .set(&DataKey::OnlyAllowStake, &only_allow_stake);
 }
 
 pub fn read_paused(e: &Env) -> bool {
     e.storage()
         .instance()
-        .get(&InstanceDataKey::Paused)
+        .get(&DataKey::Paused)
         .unwrap_or(false)
 }
 
 pub fn write_paused(e: &Env, paused: bool) {
     e.storage()
         .instance()
-        .set(&InstanceDataKey::Paused, &paused);
+        .set(&DataKey::Paused, &paused);
 }
 
 pub fn read_total_staking_amount(e: &Env) -> i128 {
     e.storage()
         .persistent()
-        .get(&PersistentDataKey::TotalStakingAmount)
+        .get(&StakingDataKey::TotalStakingAmount)
         .unwrap_or(0)
 }
 
 pub fn write_total_staking_amount(e: &Env, amount: i128) {
     e.storage()
         .persistent()
-        .set(&PersistentDataKey::TotalStakingAmount, &amount);
+        .set(&StakingDataKey::TotalStakingAmount, &amount);
 }
 
 pub fn read_total_staking_cap(e: &Env) -> i128 {
     e.storage()
         .persistent()
-        .get(&PersistentDataKey::TotalStakingCap)
+        .get(&StakingDataKey::TotalStakingCap)
         .unwrap_or(0)
 }
 
 pub fn write_total_staking_cap(e: &Env, cap: i128) {
     e.storage()
         .persistent()
-        .set(&PersistentDataKey::TotalStakingCap, &cap);
+        .set(&StakingDataKey::TotalStakingCap, &cap);
 }
 
 pub fn read_total_requested_amount(e: &Env) -> i128 {
     e.storage()
         .persistent()
-        .get(&PersistentDataKey::TotalRequestedAmount)
+        .get(&StakingDataKey::TotalRequestedAmount)
         .unwrap_or(0)
 }
 
 pub fn write_total_requested_amount(e: &Env, amount: i128) {
     e.storage()
         .persistent()
-        .set(&PersistentDataKey::TotalRequestedAmount, &amount);
+        .set(&StakingDataKey::TotalRequestedAmount, &amount);
 }
 
 pub fn read_total_claimable_amount(e: &Env) -> i128 {
     e.storage()
         .persistent()
-        .get(&PersistentDataKey::TotalClaimableAmount)
+        .get(&StakingDataKey::TotalClaimableAmount)
         .unwrap_or(0)
 }
 
 pub fn write_total_claimable_amount(e: &Env, amount: i128) {
     e.storage()
         .persistent()
-        .set(&PersistentDataKey::TotalClaimableAmount, &amount);
+        .set(&StakingDataKey::TotalClaimableAmount, &amount);
 }
 
 pub fn read_pending_stake_amount(e: &Env) -> i128 {
     e.storage()
         .persistent()
-        .get(&PersistentDataKey::PendingStakeAmount)
+        .get(&StakingDataKey::PendingStakeAmount)
         .unwrap_or(0)
 }
 
 pub fn write_pending_stake_amount(e: &Env, amount: i128) {
     e.storage()
         .persistent()
-        .set(&PersistentDataKey::PendingStakeAmount, &amount);
+        .set(&StakingDataKey::PendingStakeAmount, &amount);
 }
 
 pub fn read_collected_fee(e: &Env) -> i128 {
     e.storage()
         .persistent()
-        .get(&PersistentDataKey::CollectedFee)
+        .get(&StakingDataKey::CollectedFee)
         .unwrap_or(0)
 }
 
 pub fn write_collected_fee(e: &Env, fee: i128) {
     e.storage()
         .persistent()
-        .set(&PersistentDataKey::CollectedFee, &fee);
+        .set(&StakingDataKey::CollectedFee, &fee);
 }
 
 pub fn read_pending_unstake_time(e: &Env, user: &Address, slot: u32) -> u64 {
-    let key = TemporaryDataKey::PendingUnstakeTime(user.clone(), slot);
+    let key = PendingDataKey::PendingUnstakeTime(user.clone(), slot);
     e.storage().temporary().get(&key).unwrap_or(0)
 }
 
 pub fn write_pending_unstake_time(e: &Env, user: &Address, slot: u32, timestamp: u64) {
-    let key = TemporaryDataKey::PendingUnstakeTime(user.clone(), slot);
+    let key = PendingDataKey::PendingUnstakeTime(user.clone(), slot);
     e.storage().temporary().set(&key, &timestamp);
 }
 
 pub fn read_pending_unstake_amount(e: &Env, user: &Address, slot: u32) -> i128 {
-    let key = TemporaryDataKey::PendingUnstakeAmount(user.clone(), slot);
+    let key = PendingDataKey::PendingUnstakeAmount(user.clone(), slot);
     e.storage().temporary().get(&key).unwrap_or(0)
 }
 
 pub fn write_pending_unstake_amount(e: &Env, user: &Address, slot: u32, amount: i128) {
-    let key = TemporaryDataKey::PendingUnstakeAmount(user.clone(), slot);
+    let key = PendingDataKey::PendingUnstakeAmount(user.clone(), slot);
     e.storage().temporary().set(&key, &amount);
 }
